930313e01daaec1b9680cc3a6e56fc180852f4ce
Kieran Gill <ksgill2@dons.usfca.edu>
2018-04-08 15:31:47 -0700

    Updated titles

--- routes/music.js
+++ routes/music.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('music', { title: '7-11 Jesus' });
+  res.render('music', { title: '7-11 Jesus Music' });
 });
 
 module.exports = router;
--- routes/tour.js
+++ routes/tour.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('tour', { title: '7-11 Jesus' });
+  res.render('tour', { title: '7-11 Jesus Tour' });
 });
 
 module.exports = router;
